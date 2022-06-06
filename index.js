2e9b73556f6641582f7cf71d1cf29a0acfc0c563
Joey Faris <joeyfaris1@yahoo.com>
2022-06-06 14:14:03 -0700

    commit tree ef7f5ea5f07d34a5723d89a36bf5832701f79c7f

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
 
-// Write your code here
+const multiply = function(num1, num2) {
+    return num1 * num2
+}
+
+console.log(multiply(2, 31));
+
+
+const random = function(min, max) {
+    return Math.random() * (max-min) + min;
+
+}
+
+const mod = function(num3, num4) {
+    return console.log(num3 % num4)
+}
+
+mod(19, 5);
+
+
+const max = Math.max(1, 4, 6, 7, 20)
+
+console.log(max);
